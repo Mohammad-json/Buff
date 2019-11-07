d494fb592843be6a8ff91915b434bd8cb2c8a5c3
Levi <51336140+LeviCoding@users.noreply.github.com>
2019-11-07 22:30:09 +0200

    Update suggest.js

--- commands/other/suggest.js
+++ commands/other/suggest.js
@@ -1,7 +1,4 @@
 exports.run = async (client,message,args) => {
-  //let dUser = message.guild.member(353190313570861056);
-    //if (!dUser) return message.channel.send("Can't find user!")
- //   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't you that command!")
     let dMessage = args;
     if(dMessage.length < 1) return message.reply('You must supply a message!')
  

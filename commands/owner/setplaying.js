fd706ffff1deeef0325b45bb13b6879e4244c9f5
Levi <51336140+LeviCoding@users.noreply.github.com>
2019-11-07 22:37:36 +0200

    Update setplaying.js

--- commands/owner/setplaying.js
+++ commands/owner/setplaying.js
@@ -1,7 +1,8 @@
+//Make sure to enter your User ID!
 const Discord = require('discord.js');
 
 module.exports.run = (client, message, args) => {
-  if (message.author.id !== ('400703871086886913')) return message.channel.send("You cant use this command!");
+  if (message.author.id !== ('400703871086886915 return message.channel.send("You cant use this command!");
   const status = args.join(' ');
   if (status.length === 0) {
     const embed = new Discord.RichEmbed()

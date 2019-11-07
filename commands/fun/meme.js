dd41c99a9cfce52c02aeacd74f9718ec745325b1
Levi <51336140+LeviCoding@users.noreply.github.com>
2019-11-07 21:53:57 +0200

    Update meme.js

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -2,24 +2,7 @@
 const bot = new Discord.Client();
 const snekfetch = require('snekfetch');
 exports.run = async (client, message, args) => {
-   /* try {
-        const { body } = await snekfetch
-            .get('https://www.reddit.com/r/dankmemes.json')
-            .query({ limit: 800 });
-        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
-        if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
-        const randomnumber = Math.floor(Math.random() * allowed.length)
-        const embed = new Discord.RichEmbed()
-        .setColor(0x00A2E8)
-        .setTitle(allowed[randomnumber].data.title)
-        .setDescription("Posted by: " + allowed[randomnumber].data.author)
-        .setImage(allowed[randomnumber].data.url)
-        .addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
-        .setFooter("Memes provided by r/dankmemes")
-        message.channel.send(embed)
-    } catch (err) {
-        return console.log(err);
-    }*/
+
   let msg = message;
   const got = require('got');
 const Discord = require('discord.js');
@@ -37,7 +20,5 @@ const Discord = require('discord.js');
         embed.setImage(memeImage);
         embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`);
         msg.channel.send(embed)
-            .then(sent => console.log(`Sent a reply to ${sent.author.username}`))
-        console.log('Bot responded with: ' + memeImage);
-    }).catch(console.error);
+    })
 }

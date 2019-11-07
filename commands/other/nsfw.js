dbade0d352b6d2c75b5b14beb411ca71a00f8b2c
Levi <51336140+LeviCoding@users.noreply.github.com>
2019-11-07 22:27:55 +0200

    Update nsfw.js

--- commands/other/nsfw.js
+++ commands/other/nsfw.js
@@ -1,4 +1,3 @@
-// This command requires the discord-js package to create embeds
 const Discord = require('discord.js');
 
 exports.run = async (client, message, args, tools) => {
@@ -7,13 +6,7 @@ exports.run = async (client, message, args, tools) => {
 
     const embed = new Discord.RichEmbed()
         .setColor(0xF00000)
-        .addField('=modcmds', 'Gives all moderation commands')
-        .addField('=funcmds', 'Gives all fun commands')
-        .addField('=economycmds', 'Gives all economy commands')
-        .addField('=reactioncmds', 'Gives all reaction commands')
-        .addField('=infocmds', 'Gives all information commands')
-        .addField('=invitecmds', 'Gives all invitation commands')
-        .addField('=nsfwcmds', 'Gives all nsfw commands')
+        .addField('=nsfw', 'Some nsfw?')
         .setDescription('Categories')
         .setFooter('The bot Prefix is "="!');
 

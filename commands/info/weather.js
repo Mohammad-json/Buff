732a8202438a6a7a9178adc34f57ca67a7543214
Levi <51336140+LeviCoding@users.noreply.github.com>
2019-11-07 22:02:45 +0200

    Update weather.js

--- commands/info/weather.js
+++ commands/info/weather.js
@@ -6,24 +6,23 @@ module.exports.run = async (bot, message, args) => {
     weather.find({search: args.join(" "), degreeType: "C"}, function(err, result) {
         if(err) message.channel.send(err)
 
-        //If the place entered is invalid
         if(result.length === 1) {
             message.channel.send("**Please enter a valid location!**")
             return;
         }
 
         //Variables
-        var current = result[0].current //Variable for the current part of the JSON Output
-        var location = result[0].location //This is a variable for the location part of the JSON Output
+        var current = result[0].current
+        var location = result[0].location
 
         //Sends weather log in embed
         let embed = new Discord.RichEmbed()
-           .setDescription(`**${current.skytext}**`) //How the sky looks like
-           .setAuthor(`Weather for ${current.observationpoint}`) //Shows the current location of the weater
-           .setThumbnail(current.imageUrl) //Sets thumbnail of the embed
+           .setDescription(`**${current.skytext}**`)
+           .setAuthor(`Weather for ${current.observationpoint}`)
+           .setThumbnail(current.imageUrl)
            .setColor(0xff0000) //Sets the color of the embed
-           .addField("Timezone", `UTC${location.timezone}`, true) //Shows the timezone
-           .addField("Degree Type", location.degreetype, true) //Shows the degrees in Celcius
+           .addField("Timezone", `UTC${location.timezone}`, true)
+           .addField("Degree Type", location.degreetype, true)
            .addField("Temperature", `${current.temperature}`, true)
            .addField("Feels like", `${current.feelslike} Degrees`, true)
            .addField("Winds", current.winddisplay, true)
@@ -31,7 +30,6 @@ module.exports.run = async (bot, message, args) => {
            .addField("Day", `${current.day}`, true)
            .addField("Date", `${current.date}`, true)
            
-           //Display when it's called
            message.channel.sendEmbed(embed)
 
     });

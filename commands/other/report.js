764cd209ca284660ee40a643d7f0a3f6037a5900
Levi <51336140+LeviCoding@users.noreply.github.com>
2019-11-07 22:29:05 +0200

    Update report.js

--- commands/other/report.js
+++ commands/other/report.js
@@ -7,18 +7,13 @@ module.exports = {
     },
     run: async (bot, message, args) => {
 
-        // mentioned or grabbed user
         let target = message.mentions.members.first() || message.guild.members.get(args[0])
         if(!target) return message.channel.send("Please provide a valid user! Note: you must have channel called reports to get all reports from members!")
 
-        // reasoning definition
         let reason = args.slice(1).join(" ")
         if(!reason) return message.channel.send(`Please provide a reason for reporting **${target.user.tag}**!`)
 
-        // grab reports channel
         let sChannel = message.guild.channels.find(x => x.name === "reports")
-
-        // send to reports channel and add tick or cross
 
         message.channel.send("Your report has been filled to the staff team. Thank you!").then(m => m.delete(15000))
         sChannel.send(`**${message.author.tag}** has reported **${target.user.tag}** for **${reason}**. Should we ban/kick/mute him?`).then(async msg => {

82f0f5a7e81aeca33463661300ae9221742927ad
Levi <51336140+LeviCoding@users.noreply.github.com>
2019-11-07 21:55:47 +0200

    Update ping.js

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -1,6 +1,5 @@
 exports.run = async (client, message, args) => {
-  // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-  // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+
   const Discord = require("discord.js");
   function ping(host, pong) {
  
@@ -23,33 +22,14 @@ exports.run = async (client, message, args) => {
   try {
     http.send(null);
   } catch(exception) {
-    // this is expected
   }
  
 }
 if (!args[0]) {
-  // inside a command, event listener, etc.
   const exampleEmbed = new Discord.RichEmbed()
     .setColor("0xff0000")
     .setTitle("Loading...")
-    /*.setURL("https://discord.js.org/")
-    .setAuthor(
-      "Some name",
-      "https://i.imgur.com/wSTFkRM.png",
-      "https://discord.js.org"
-    )
-    .setDescription("Some description here")
-    .setThumbnail("https://i.imgur.com/wSTFkRM.png")
-    .addField("Regular field title", "Some value here")
-    .addBlankField()
-    .addField("Inline field title", "Some value here", true)
-    .addField("Inline field title", "Some value here", true)
-    .addField("Inline field title", "Some value here", true)
-    .setImage("https://i.imgur.com/wSTFkRM.png")
-    .setTimestamp()
-    .setFooter("Some footer text here", "https://i.imgur.com/wSTFkRM.png");*/
  
- // channel.send(exampleEmbed);
   const m = await message.channel.send(exampleEmbed);
   const noobEmbed = new Discord.RichEmbed()
   .setColor("0xff0000")
@@ -57,8 +37,6 @@ if (!args[0]) {
   .setDescription(`Latency: ${m.createdTimestamp - message.createdTimestamp}ms. \nDiscord API: ${Math.round(client.ping)}ms`)
   .setTimestamp();
  
- 
-  //const m = await message.channel.send(exampleEmbed);
-  m.edit(noobEmbed);
+   m.edit(noobEmbed);
 }
 };
